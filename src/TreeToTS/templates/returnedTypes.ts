19d233c9d3cf727a6f0561c838fb88d367bc5774
Niko Oshinov <niko.oshinov@gmail.com>
2022-03-21 11:40:19 -0400

    Reverted const enum change to 'enum' https://github.com/graphql-editor/graphql-zeus/pull/210/files

--- src/TreeToTS/templates/returnedTypes.ts
+++ src/TreeToTS/templates/returnedTypes.ts
@@ -60,7 +60,8 @@ const resolveEnum = (i: ParserField): string => {
   if (!i.args) {
     throw new Error('Empty enum error');
   }
-  return `${plusDescription(i.description)}export const enum ${i.name} {\n${i.args
+  // ProductWind: Reverts https://github.com/graphql-editor/graphql-zeus/pull/210/files
+  return `${plusDescription(i.description)}export enum ${i.name} {\n${i.args
     .map((f) => `\t${f.name} = "${f.name}"`)
     .join(',\n')}\n}`;
 };
